89a4c4d5a2629b074d010c81792a937398092542
Evans Gacheru <evans.gacheru.munene@gmail.com>
2025-07-25 15:05:13 +0300

    swagger documentation setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('RSV Backend Assessment')
+    .setDescription('Products API Documentation')
+    .setVersion('1.0 ')
+    .addTag('Nestjs')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
